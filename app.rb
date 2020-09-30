7851fc539abfb6d198ff0df9c9b68654e657bbcb
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-30 05:38:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @item = params[:item]
+    
+      erb :index
+  end
 end
